79f2551fb56e54e940410bfc43bd815c73eb3607
estelita987 <axelestelita97@gmail.com>
2024-01-14 12:13:05 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,15 @@
+/*import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+/*
+function CartWidget ({number,clickEnCarrito}) {
+    return(
+        <div className='containerCart'>
+            <ShoppingCartIcon
+                className="cartIcon"
+                onClick={()=>clickEnCarrito("Carrito")}
+            />
+            <span className='cartNumber'>{number}</span>
+            </div>
+    );
+}
+export default CartWidget;
+*/
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,24 @@
+import "./navBarStyle.css";
+
+/*
+function Navbar () {
+    const handleClick = (seccion) =>{
+        console.log(seccion);
+    };
+    return(
+        <div className="navBar">
+            <img src={Logo} alt="logo" className="logo"/>
+            <CartWidget number={2} clickEnCarrito={clickEnCarrito}/>
+        </div>
+    );
+}
+export default Navbar;
+*/
+function Navbar () {
+    return (
+        <div className="navBar">
+            <h1>Hola</h1>
+        </div>
+    )
+}
+export default Navbar;
