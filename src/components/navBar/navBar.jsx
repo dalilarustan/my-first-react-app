bf7b7d9cd960560a43dd148297563ee7e01ae423
Dalila <dalilarustan@gmail.com>
2024-01-18 13:28:58 -0300

    agregue container y secciones en navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,6 +1,7 @@
 import "./navBarStyle.css";
 import CartWidget from "../CartWidget/CartWidget";
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/logo.png";
+import {useState} from 'react'
 
 /*
 function Navbar () {
@@ -17,9 +18,19 @@ function Navbar () {
 export default Navbar;
 */
 function Navbar () {
+    const [seccionActual, setSeccionActual] = useState('inicio');
+    const handleClick = (seccion)=>{
+        setSeccionActual(seccion);
+        console.log(`Usted se encuentra en la sección: ${seccion}`)
+    }
     return (
         <div className="navBar">
             <img src={Logo} alt="logo" className="logo1"/>
+            <div>
+                <button onClick={()=> handleClick('plantas interior')}>plantas de interior</button>
+                <button onClick={()=> handleClick('plantas exterior')}>plantas de exterior</button>
+                <button onClick={()=> handleClick('cuidado')}>cuidado</button>
+            </div>
             <CartWidget number={3}/>
 
         </div>
