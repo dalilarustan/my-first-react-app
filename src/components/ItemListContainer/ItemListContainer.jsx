d33cc5b3bf363f2f479a7844c80f9ea07e41c1c8
Dalila <dalilarustan@gmail.com>
2024-01-28 19:43:12 -0300

    agregue productos al container

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-import "./ItemLiContainerStyle.css"
+import React, { useState } from "react";
+import "./ItemLiContainerStyle.css";
+import { useEffect } from "react";
+import { getProducts } from "../../serverProducts/productMock";
 
 function ItemListContainer ({greeting}){
+    const [items, setItems]=useState([])
+    useEffect(()=>{
+        getProducts().then((res)=>setItems(res));
+    },[])
     return(
         <div className="containerStyle">
             <h1>{greeting}</h1>
+            <div className="listItems">
+                {items.map((item)=>{
+                    const {id, nombre, precio, cantidad, categoria, img} = item
+                    return(
+                        <div key={id} className="item">
+                            <img src={img} alt={nombre}></img>
+                            <h3>{nombre}</h3>
+                            <p>{precio}</p>
+                            <p>{cantidad}</p>
+                            <p>{categoria}</p>
+
+                        </div>
+
+                    )
+                })}
+            </div>
         </div>
     )
 }
